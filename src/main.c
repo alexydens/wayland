2a81641e6a0eff7bd0a29f2f1cf7853204ec1723
alexydens <alexydens@outlook.com>
2024-04-19 20:12:18 +0100

    ...

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 
 /* stdlib headers */
 #include <string.h>
+#include <stdio.h>
 
 /* Entry point */
 int main(void) {
@@ -18,7 +19,10 @@ int main(void) {
     u16 w, h;
     u32* framebuffer = get_window_buffer(window);
     get_window_dimensions(window, &w, &h);
-    memset(framebuffer, 0xff, w * h * 4);
+    memset(framebuffer, 0x00, w * h * 4);
+    for (u32 i = 0; i < w * h; i++) {
+      framebuffer[i] = 0xff00ff00;
+    }
   }
   /* Destroy window */
   destroy_window(window);
